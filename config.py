fae96fd0d9bce08b190fbfd2c5ea801f7835d9d0
bgieta <bgieta@bgieta.lan>
2026-05-02 11:13:23 +0200

    chore: auto-update from hub

--- config.py
+++ config.py
@@ -9,4 +9,16 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 def get_conn_string():
-    return f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={DB_SERVER};DATABASE={DB_NAME};UID={DB_USER};PWD={DB_PASSWORD};TrustServerCertificate=yes;'
+    # Force ODBC Driver 18 with security overrides for local/dev environments
+    # Added MultiSubnetFailover=Yes for better networking in some Docker setups
+    return (
+        f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+        f'SERVER={DB_SERVER};'
+        f'DATABASE={DB_NAME};'
+        f'UID={DB_USER};'
+        f'PWD={DB_PASSWORD};'
+        'Encrypt=no;'
+        'TrustServerCertificate=yes;'
+        'Connection Timeout=60;'
+        'MultiSubnetFailover=No;'
+    )
